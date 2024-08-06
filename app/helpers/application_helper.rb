1a11b7402b074295645af6aeb442e993a028efd5
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2024-08-06 15:57:45 -0300

    Update broadcast targets and add count updates for tasks and projects

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   def completed_count(model)
     count = model.completed_count
     color_class = count > 0 ? "bg-green-500" : "bg-red-500"
-    content_tag(:span, "(#{count})", class: "#{color_class} ml-2 p-1 rounded-full")
+    content_tag(:span, "#{count}", class: "#{color_class} ml-2 p-1 rounded-full w-10 h-10 text-center flex items-center justify-center")
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -15,11 +15,14 @@ class Task < ApplicationRecord
   include Completable
   validates :title, presence: true
 
-  after_create_commit { broadcast_prepend_to 'tasks', target: 'tasks', partial: 'tasks/task', locals: { task: self }}
+  after_create_commit { broadcast_prepend_to 'tasks', target: 'results', partial: 'tasks/task', locals: { task: self }}
 
   after_update_commit { broadcast_update_to "tasks", target: "task_#{self.id}", partial: "tasks/task", locals: { task: self } }
 
   after_destroy_commit { broadcast_remove_to "tasks", target: "task_#{self.id}" }
+
+  after_save { broadcast_update_to "update_counts", target: "tasks_frame", partial: "shared/tasks_count" }
+  after_destroy { broadcast_update_to "update_counts", target: "tasks_frame", partial: "shared/tasks_count" }
 
   has_one_attached :feature_image
 end
